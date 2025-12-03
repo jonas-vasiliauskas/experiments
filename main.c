6c2236da7e2209c681d3b3d88d36ee052034729b
Jonas Vasiliauskas <vas.jonas@gmail.com>
2025-12-03 19:21:20 +0200

    Pirma programos versija

--- main.c
+++ main.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+
 int main(){
+    int first_number,second_number;
+    puts("Iveskite pirma skaiciu");
+    scanf("%i",&first_number);
+    puts("Iveskite antra skaiciu");
+    scanf("%i",&second_number);
+    const int sum = first_number + second_number;
+    printf("%s%i\n","suma ",sum);
     return 0;
 }
