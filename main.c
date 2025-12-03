34d40f456ef7b8874efdc04996053c15bd910c98
Jonas Vasiliauskas <vas.jonas@gmail.com>
2025-12-03 20:20:12 +0200

    Antra  programos versija. Apsaugota nuo blogu duomenu ivedimo

--- main.c
+++ main.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int get_int(const char *msg){
+    const int BUFFER_SIZE =  100;
+    bool isEqual = false;
+    
+    int result;
+    char buffer1[BUFFER_SIZE],buffer2[BUFFER_SIZE];
+    do{
+        memset(buffer1,0,BUFFER_SIZE);
+        memset(buffer2,0,BUFFER_SIZE);
+        puts(msg);
+        scanf("%s",buffer1);
+        result=atoi(buffer1);
+        snprintf(buffer2,BUFFER_SIZE,"%i",result);
+        isEqual=strcmp(buffer1,buffer2)==0;
+        if (!isEqual)
+            puts("Klaida");
+    }
+    while(!isEqual);
+    return result;
+}
 
 int main(){
     int first_number,second_number;
-    puts("Iveskite pirma skaiciu");
-    scanf("%i",&first_number);
-    puts("Iveskite antra skaiciu");
-    scanf("%i",&second_number);
+    first_number=get_int("Iveskite pirma skaiciu");
+    second_number=get_int("Iveskite antra skaiciu");
     const int sum = first_number + second_number;
     printf("%s%i\n","suma ",sum);
     return 0;
