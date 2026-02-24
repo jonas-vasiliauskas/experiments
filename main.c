f1687b50320f4a54e2ed8f50efc5426767174a84
Jonas Vasiliauskas <vas.jonas@gmail.com>
2026-02-24 19:13:35 +0200

    get_file_statistics(const char*, const int, int**) nebeluzta 98 eiluteje

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int get_int(const char *msg,const char *err_msg){
 
 double get_double(const char *msg,const char *err_msg){
     const int BUFFER_SIZE = 100;
-    bool isDouble;
+    bool isDouble=false;
     int i;
     
     double result;
@@ -55,39 +55,51 @@ double get_double(const char *msg,const char *err_msg){
 
 void test3(int **val){
     int i;
-    *val = malloc(sizeof(int)*ARRAY_SIZE);
+    *val = (int*)malloc(sizeof(int)*ARRAY_SIZE);
     for (i=0;i<ARRAY_SIZE;++i)
         (*val)[i]=i*i;
 }
 
-int main(){
-    int i,*test_value;
-    test3(&test_value);
-    for (i=0;i<ARRAY_SIZE;++i){
-        printf("%6i ",test_value[i]);
-        if (i % 10 == 0 && i!=0)
-            puts("");
-    }
-    free(test_value);
-}
 
-
-/*int get_file_statistics(const char *file_name,const int word_size, int **word_freq_stats){
+int get_file_statistics(const char *file_name,const int word_size, int **word_freq_stats){
     FILE *data_file = fopen(file_name,"rb");
     if (data_file == NULL)
         return 1;
     if (word_size < 1 || word_size > 16)
         return 2;
-        
-    const int READ_BLOCK_SIZE = 4096;
-    const int WORD_COUNT = 1 << word_size;
-    int i;
-    char read_block[READ_BLOCK_SIZE];
-    memset(read_block,0,READ_BLOCK_SIZE);
     
-    *word_freq_stats =(int**) malloc (sizeof(int*)*WORD_COUNT);
-  //  for (i=0;i<WORD_COUNT;++i)
-  //      *word_freq_stats[i]=1;
+    const int BITS_IN_BYTE = 8;    
+    const int READ_BLOCK_SIZE = 4096;
+    const int EXPANDED_READ_BLOCK_SIZE = READ_BLOCK_SIZE*BITS_IN_BYTE;
+    const int WORD_COUNT = 1 << word_size;
+    int i,j,byte_count = READ_BLOCK_SIZE;
+    char read_block[READ_BLOCK_SIZE],expanded_read_block[EXPANDED_READ_BLOCK_SIZE];
+    
+    *word_freq_stats =(int*) malloc (sizeof(int)*WORD_COUNT);
+    if (word_freq_stats == NULL)
+        return 3;
+        
+    
+    while (byte_count == READ_BLOCK_SIZE){
+        memset(read_block,0,READ_BLOCK_SIZE);
+        memset(expanded_read_block,0,EXPANDED_READ_BLOCK_SIZE);
+        byte_count=fread(read_block,sizeof(char),READ_BLOCK_SIZE,data_file);
+        for (i=0;i<byte_count;++i)
+            for (j=1;j<BITS_IN_BYTE;++j)
+                expanded_read_block[i*BITS_IN_BYTE+j]=(read_block[i]>>(BITS_IN_BYTE-j-1))%2;
+        
+        int bit_counter=0,word_value=0;
+        for (i=0;i<byte_count*BITS_IN_BYTE;++i)
+            if (bit_counter<word_size){
+                word_value=(word_value<<1)+expanded_read_block[i];
+                ++bit_counter;
+            }
+            else{
+                ++(*word_freq_stats)[word_value];
+                bit_counter=0;
+                word_value=0;
+            }
+    }
    
     fclose(data_file);
     return 0;
@@ -104,7 +116,7 @@ int main(){
     int i;
     
     for (i=0;i<WORD_COUNT;++i)
-        printf("%i",word_freq_stats[i]);
+        printf("%i ",word_freq_stats[i]);
     free(word_freq_stats);
     return 0;
-}*/
+}
