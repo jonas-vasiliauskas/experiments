3e1a6ecc467a8a8925ce27604e4d8802fdd7a260
Jonas Vasiliauskas <vas.jonas@gmail.com>
2026-03-08 23:20:25 +0200

    ivedus 'q' gijos baigia darba

--- main2.cpp
+++ main2.cpp
@@ -1,15 +1,29 @@
+#include <iostream>
+#include <string>
 #include <vector>
 #include <thread>
 
-void task(){
-   
+void task(bool *isRunning){
+   int i,c=0;
+   while (*isRunning)
+   for (i=0;i<1000;++i)
+       c = (c + c * c) % 256;
 }
 
 int main(){
+    bool isRunning=true;
     const int CPU_COUNT = std::thread::hardware_concurrency();
     std::vector<std::thread> taskThreads;
     for (int i=0;i<CPU_COUNT;++i)
-        taskThreads.push_back(std::thread(task));
+        taskThreads.push_back(std::thread(task,&isRunning));
     for (int i=0;i<CPU_COUNT;++i)
-        taskThreads[i].detach();        
+        taskThreads[i].detach();
+    while (true){
+        std::string inputString;    
+        std::cin>>inputString;
+        if (inputString=="q"){
+            isRunning = false;
+            break;
+        }
+    }        
 }
