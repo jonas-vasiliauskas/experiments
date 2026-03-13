373d5b7a0a745fe93b869be7b7e9046b401de018
Jonas Vasiliauskas <vas.jonas@gmail.com>
2026-03-13 11:20:23 +0200

    main2.cpp: galima nustatyti darbo ir poilsio periodu trukme(2)

--- main2.cpp
+++ main2.cpp
@@ -3,12 +3,12 @@
 #include <vector>
 #include <thread>
 #include <chrono>
-#include <climits>
 
-int task(bool *isRunning,const int sleepInMs,const int workInMs){
+int task(bool *isRunning,const int sleepInMs,const int workInMs,int count){
    int i,c=0;
    time_t startTime = time(NULL);
-   while (*isRunning){
+   while (*isRunning && (count > 0 || count != -1)){
+       --count;
        time_t endTime=startTime;
        while (difftime(endTime,startTime)*1000<workInMs){
            for (i=0;i<1000;++i)
@@ -18,11 +18,21 @@ int task(bool *isRunning,const int sleepInMs,const int workInMs){
        startTime = time(NULL);
        std::this_thread::sleep_for(std::chrono::milliseconds(sleepInMs));
    }
+   std::cout<<"Finished thread\n";
    return c;
 }
 
+void killerTask(bool *isRunning){
+    while (!(*isRunning)){
+        std::string inputOption;
+        std::cin>>inputOption;
+        if (inputOption=="q")
+            *isRunning=false;
+    }
+}
+
 int main(int argC,char **argV){
-    if (argC!=3){
+    if (argC!=4){
          std::cerr<<"Wrong argument count\n";
          return 1;
     }
@@ -31,6 +41,7 @@ int main(int argC,char **argV){
     std::vector<std::thread> taskThreads;
     const int SLEEP_IN_MS= std::stoi(argV[1])*1000;
     const int WORK_IN_MS = std::stoi(argV[2])*1000;
+    const int REPETITION_COUNT = std::stoi(argV[3]);
     
     if (WORK_IN_MS <= SLEEP_IN_MS){
         std::cerr<<"Work period must be longer than sleep one\n";
@@ -38,15 +49,9 @@ int main(int argC,char **argV){
     }
         
     for (int i=0;i<CPU_COUNT;++i)
-        taskThreads.push_back(std::thread(task,&isRunning,SLEEP_IN_MS,WORK_IN_MS));
+        taskThreads.push_back(std::thread(task,&isRunning,SLEEP_IN_MS,WORK_IN_MS,REPETITION_COUNT));
+    std::thread killerThread(killerTask,&isRunning);
     for (int i=0;i<CPU_COUNT;++i)
-        taskThreads[i].detach();
-    while (true){
-        std::string inputString;    
-        std::cin>>inputString;
-        if (inputString=="q"){
-            isRunning = false;
-            break;
-        }
-    }        
+        taskThreads[i].join();
+    killerThread.join();        
 }
